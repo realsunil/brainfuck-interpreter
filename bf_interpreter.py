c2f5469e7b4dd01c2e0f9db8cf6749ce77e3aa3f
Sunil <72736620+sunilhaslinux@users.noreply.github.com>
2021-04-16 18:29:44 +0530

    Add files via upload

---
+++ bf_interpreter.py
@@ -0,0 +1,70 @@
+import sys
+
+cells = [0]
+ptr_loc = 0
+
+if(len(sys.argv) < 2):
+	print("Give file name as command line argument")
+	exit(0)
+
+file_handle = open(sys.argv[1], "r")
+
+code = file_handle.read()
+i=0
+while i<len(code):
+	#print(i,end="")
+	if code[i] == '<':
+		ptr_loc -= 1
+		if(ptr_loc < 0):
+			print("RANGE ERROR")
+			exit(1)
+
+	elif code[i] == '>':
+		ptr_loc += 1
+		# print(len(cells))
+		# print(ptr_loc)
+		if(len(cells) < ptr_loc+1):
+			cells.extend([0])
+		if(ptr_loc > 30000):
+			print("RANGE ERROR")
+			exit(1)
+
+	elif code[i] == '.':
+		try:
+			print(chr(cells[ptr_loc]), end="")
+		except ValueError:
+			print("Not in ASCII range")
+
+	elif code[i] == ',':
+		try:
+			cells[ptr_loc] = ord(input())
+		except TypeError:
+			print("Single Char Was Expected")
+
+	elif code[i] == '+':
+		cells[ptr_loc] += 1
+
+	elif code[i] == '-':
+		cells[ptr_loc] -= 1
+
+	elif code[i] == '[':
+		if cells[ptr_loc] == 0:
+			open_braces = 1
+			while open_braces > 0:
+				i += 1
+				if code[i] == '[':
+					open_braces += 1
+				elif code[i] == ']':
+					open_braces -= 1
+
+	elif code[i] == ']':
+		open_braces = 1
+		while open_braces > 0:
+			i -= 1
+			if code[i] == '[':
+				open_braces -= 1
+			elif code[i] == ']':
+				open_braces += 1
+		# i still gets incremented in your main while loop
+		i -= 1
+	i += 1
